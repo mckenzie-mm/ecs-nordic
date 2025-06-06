70af55337f3494c6d728fe417bdb311cf5d22561
mark <mmackenzie.uk@gmail.com>
2025-06-07 09:42:56 +1000

    seed database at startup

--- nordic-api/app/Program.cs
+++ nordic-api/app/Program.cs
@@ -4,7 +4,14 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 {
     // Add services to the container.
     var DB_CONNECTION_STRING = builder.Configuration["DB_CONNECTION_STRING"];
-    builder.Services.AddSingleton(_ => new SeedService(DB_CONNECTION_STRING));
+
+    var db = new SeedService(DB_CONNECTION_STRING);
+    if (db != null)
+    {
+        await db.Seed();
+        builder.Services.AddSingleton(db);
+    }
+    
     builder.Services.AddSingleton(_ => new CategoriesService(DB_CONNECTION_STRING));
     builder.Services.AddSingleton(_ => new ProductsService(DB_CONNECTION_STRING));
     builder.Services.AddControllers();
@@ -17,3 +24,4 @@ WebApplication app = builder.Build();
 }
 
 app.Run();
+
